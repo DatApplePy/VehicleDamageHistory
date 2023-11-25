492f21940d322c5e1cb9763a0018242f4956f323
DatApplePy <szohrfelix@outlook.hu>
2023-11-25 12:51:01 +0100

    Add relationships to models

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     use HasFactory;
+
+    public function damageEvents(){
+        return $this->belongsToMany(DamageEvent::class);
+    }
 }
